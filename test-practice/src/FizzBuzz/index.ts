a9fe9a41c42b50db909ac0896be56ff5b12f7d5d
Denovo <vo0v0000@naver.com>
2023-12-15 15:07:17 +0900

    feat(TDD_연습): isPrime 개발 - 테스트통과

--- test-practice/src/FizzBuzz/index.ts
+++ test-practice/src/FizzBuzz/index.ts
@@ -25,12 +25,12 @@ const fizzBuzzFactory: FizzBuzzFactory = () => {
 export default fizzBuzzFactory;
 
 export function isPrime(num: number): boolean {
-  //   if (num === 1) return false;
-  //   if (num === 2) return true;
+  if (num === 1) return false;
+  if (num === 2) return true;
 
-  //   for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
-  //     if (num % i === 0) return false;
-  //   }
+  for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
 
   return true;
 }
