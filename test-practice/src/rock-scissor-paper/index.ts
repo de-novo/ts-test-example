fbb6fa1be6fd0b19dcf294c11aee66e470cb0f82
Denovo <vo0v0000@naver.com>
2023-12-15 11:50:48 +0900

    feat(TDD_연습): 가위 바위 보 개발(테스트통과)

--- test-practice/src/rock-scissor-paper/index.ts
+++ test-practice/src/rock-scissor-paper/index.ts
@@ -10,8 +10,11 @@ type createRockScissorPaper = () => RockScissorPaper;
 
 const rockScissorPaper: RockScissorPaper = (player1, player2) => {
   if (player1 === player2) return "draw";
+  if (player1 === "rock" && player2 === "scissor") return "player1 win";
+  if (player1 === "scissor" && player2 === "paper") return "player1 win";
+  if (player1 === "paper" && player2 === "rock") return "player1 win";
 
-  return "player1 win";
+  return "player1 lose";
 };
 
 const createRockScissorPaper: createRockScissorPaper = () => {
