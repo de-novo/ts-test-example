c1cf809d5281b25073c495c57e6cd9a98e16896a
Denovo <vo0v0000@naver.com>
2023-12-15 12:01:39 +0900

    test(TDD_연습): 가위바위보 테스트 Arrange개선(중복제거)

--- test-practice/test/rock-scissor-paper/index.test.ts
+++ test-practice/test/rock-scissor-paper/index.test.ts
@@ -1,37 +1,35 @@
 import createRockScissorPaper from "@src/rock-scissor-paper";
 
 describe("rock-scissor-paper", () => {
+  let rockSissorPaperGame: ReturnType<typeof createRockScissorPaper>;
+
+  //Arrange
+  beforeEach(() => {
+    rockSissorPaperGame = createRockScissorPaper();
+  });
+
   describe("play", () => {
     describe("output: draw", () => {
       const expected = "draw";
       it("player1 rock - player2 rock: draw", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
-
         // Act
-        const actual = sut("rock", "rock");
+        const actual = rockSissorPaperGame("rock", "rock");
 
         // Assert
         expect(actual).toEqual(expected);
       });
 
       it("player1 scissor - player2 scissor: draw", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
-
         // Act
-        const actual = sut("scissor", "scissor");
+        const actual = rockSissorPaperGame("scissor", "scissor");
 
         // Assert
         expect(actual).toEqual(expected);
       });
 
       it("player1 paper - player2 paper: draw", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
-
         // Act
-        const actual = sut("paper", "paper");
+        const actual = rockSissorPaperGame("paper", "paper");
 
         // Assert
         expect(actual).toEqual(expected);
@@ -42,33 +40,24 @@ describe("rock-scissor-paper", () => {
       const expected = "player1 win";
 
       it("player1 rock - player2 scissor: player1 win", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
-
         // Act
-        const actual = sut("rock", "scissor");
+        const actual = rockSissorPaperGame("rock", "scissor");
 
         // Assert
         expect(actual).toEqual(expected);
       });
 
       it("player1 scissor - player2 paper: player1 win", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
-
         // Act
-        const actual = sut("scissor", "paper");
+        const actual = rockSissorPaperGame("scissor", "paper");
 
         // Assert
         expect(actual).toEqual(expected);
       });
 
       it("player1 paper - player2 rock: player1 win", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
-
         // Act
-        const actual = sut("paper", "rock");
+        const actual = rockSissorPaperGame("paper", "rock");
 
         // Assert
         expect(actual).toEqual(expected);
@@ -78,32 +67,24 @@ describe("rock-scissor-paper", () => {
     describe("output: player1 lose", () => {
       const expected = "player1 lose";
       it("player1 rock - player2 paper: player1 lose", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
-
         // Act
-        const actual = sut("rock", "paper");
+        const actual = rockSissorPaperGame("rock", "paper");
 
         // Assert
         expect(actual).toEqual(expected);
       });
 
       it("player1 scissor - player2 rock: player1 lose", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
         // Act
-        const actual = sut("scissor", "rock");
+        const actual = rockSissorPaperGame("scissor", "rock");
 
         // Assert
         expect(actual).toEqual(expected);
       });
 
       it("player1 paper - player2 scissor: player1 lose", () => {
-        // Arrange
-        const sut = createRockScissorPaper();
-
         // Act
-        const actual = sut("paper", "scissor");
+        const actual = rockSissorPaperGame("paper", "scissor");
 
         // Assert
         expect(actual).toEqual(expected);
