253321eae3ad79ec5e7eeddfaaa86dd7c52fd54e
Denovo <vo0v0000@naver.com>
2023-12-16 11:45:11 +0900

    test(TDD_연습): age calculator - same month, same day 테스트 추가 및 기존 테스트 설명 변경

--- test-practice/test/age-calculator/index.test.ts
+++ test-practice/test/age-calculator/index.test.ts
@@ -30,37 +30,87 @@ describe("age-calculator", () => {
   });
 
   describe("calculateAge each", () => {
-    const testCases = [
-      // now, birthDate, expected
-      ["2020-01-01", "1990-01-01", 30],
-      ["2020-01-01", "1990-01-02", 29],
-      ["2020-01-01", "1990-02-01", 29],
-      ["2020-01-01", "1991-01-01", 29],
-      ["2020-01-01", "1989-12-31", 30],
-      ["2020-01-01", "1990-01-01", 30],
-      ["2020-01-01", "1990-01-02", 29],
-      ["2020-01-01", "1990-02-01", 29],
-      // 윤년
-      ["2020-02-29", "1990-02-28", 30],
-      ["2020-02-29", "1990-03-01", 29],
-      ["2021-02-28", "2020-02-29", 0],
-      ["2021-02-28", "2020-03-01", 0],
-      ["2021-03-01", "2020-02-29", 1],
-      ["2021-03-01", "2020-03-01", 1],
-    ];
+    describe("same month", () => {
+      const testCases = [
+        // now, birthDate, expected
+        ["2020-01-01", "2020-01-01", 0],
+        ["2020-01-01", "1990-01-01", 30],
+        ["2020-01-01", "1990-01-02", 29],
+        ["2020-01-01", "1990-01-31", 29],
+        ["2020-12-31", "1990-12-01", 30],
+        ["2020-12-31", "1990-12-02", 30],
+        ["2020-12-31", "1990-12-31", 30],
+      ];
 
-    test.each(testCases)(
-      "now: %s, birthDate: %s :: age %i",
-      (nowStr, birthDateStr, expected) => {
-        // Arrange
-        now = new Date(nowStr);
-        birthDate = new Date(birthDateStr);
+      test.each(testCases)(
+        "now: %s, birthDate: %s :: age %i",
+        (nowStr, birthDateStr, expected) => {
+          // Arrange
+          now = new Date(nowStr);
+          birthDate = new Date(birthDateStr);
 
-        // Act
-        const age = ageCalculator(birthDate, now);
-        // Assert
-        expect(age).toBe(expected);
-      }
-    );
+          // Act
+          const age = ageCalculator(birthDate, now);
+          // Assert
+          expect(age).toBe(expected);
+        }
+      );
+    });
+
+    describe("same day", () => {
+      const testCases = [
+        ["2020-01-01", "2020-01-01", 0],
+        ["2020-01-01", "1990-01-01", 30],
+        ["2020-02-01", "1990-02-01", 30],
+        ["2020-01-01", "1990-02-01", 29],
+      ];
+      test.each(testCases)(
+        "now: %s, birthDate: %s :: age %i",
+        (nowStr, birthDateStr, expected) => {
+          // Arrange
+          now = new Date(nowStr);
+          birthDate = new Date(birthDateStr);
+
+          // Act
+          const age = ageCalculator(birthDate, now);
+          // Assert
+          expect(age).toBe(expected);
+        }
+      );
+    });
+    describe("random tests", () => {
+      const testCases = [
+        // now, birthDate, expected
+        ["2020-01-01", "1990-01-01", 30],
+        ["2020-01-01", "1990-01-02", 29],
+        ["2020-01-01", "1990-02-01", 29],
+        ["2020-01-01", "1991-01-01", 29],
+        ["2020-01-01", "1989-12-31", 30],
+        ["2020-01-01", "1990-01-01", 30],
+        ["2020-01-01", "1990-01-02", 29],
+        ["2020-01-01", "1990-02-01", 29],
+        // 윤년
+        ["2020-02-29", "1990-02-28", 30],
+        ["2020-02-29", "1990-03-01", 29],
+        ["2021-02-28", "2020-02-29", 0],
+        ["2021-02-28", "2020-03-01", 0],
+        ["2021-03-01", "2020-02-29", 1],
+        ["2021-03-01", "2020-03-01", 1],
+      ];
+
+      test.each(testCases)(
+        "now: %s, birthDate: %s :: age %i",
+        (nowStr, birthDateStr, expected) => {
+          // Arrange
+          now = new Date(nowStr);
+          birthDate = new Date(birthDateStr);
+
+          // Act
+          const age = ageCalculator(birthDate, now);
+          // Assert
+          expect(age).toBe(expected);
+        }
+      );
+    });
   });
 });
