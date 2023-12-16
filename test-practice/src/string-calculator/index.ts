69e8fa948b9a2238d037df4ffafb5cd5f4f2591d
Denovo <vo0v0000@naver.com>
2023-12-16 13:48:54 +0900

    feat(TDD_연습): string calculator - 테스트 통과 코드 작성

--- test-practice/src/string-calculator/index.ts
+++ test-practice/src/string-calculator/index.ts
@@ -4,6 +4,27 @@ export interface StringCalculator {
 
 export class StringCalculator implements StringCalculator {
   add(input: string): number {
-    return 0;
+    if (input === "") {
+      return 0;
+    }
+    const isCustomSplitter = input.startsWith("//");
+
+    const customsplitter = isCustomSplitter
+      ? input.split("\n")[0].replace("//", "")
+      : /,|:/;
+
+    //  , or :
+    const numbers = isCustomSplitter
+      ? input
+          .split("\n")[1]
+          .split(customsplitter)
+          .map((number) => parseInt(number))
+      : input.split(customsplitter).map((number) => parseInt(number));
+
+    const result = numbers.reduce((acc, cur) => acc + cur, 0);
+    if (isNaN(result)) {
+      throw new Error("문자열에 숫자 이외의 값이 포함되어 있습니다.");
+    }
+    return result;
   }
 }
