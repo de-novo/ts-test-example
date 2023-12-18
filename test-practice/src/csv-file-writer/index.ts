c693d8a90a5aa94c91aae1da89ff9ba19189d739
Denovo <vo0v0000@naver.com>
2023-12-18 14:50:47 +0900

    feat(TDD_연습): csv file writer - 테스트 통과 코드 작성 및, 테스트 추가

--- test-practice/src/csv-file-writer/index.ts
+++ test-practice/src/csv-file-writer/index.ts
@@ -7,5 +7,21 @@ export interface Fs {
 export class CsvFileWriter {
   constructor(private readonly fs: Fs) {}
 
-  writeCsvFile<T>(path: string, data: T[]): void {}
+  writeCsvFile<T extends object>(
+    path: string,
+    data: T[],
+    headers?: (keyof T)[]
+  ): void {
+    if (data.length === 0) throw new Error("data is empty");
+
+    const header = headers ?? Object.keys(data[0]);
+
+    const headerString = header.join(",");
+    const dataString = data
+      .map((d) => header.map((h) => d[h]).join(","))
+      .join("\n");
+    const csvString = `${headerString}\n${dataString}`;
+
+    this.fs.writeFileSync(path, csvString);
+  }
 }
--- test-practice/test/csv-file-writer/index.test.ts
+++ test-practice/test/csv-file-writer/index.test.ts
@@ -18,23 +18,147 @@ import { CsvFileWriter, Fs } from "@src/csv-file-writer";
  * 3, jun, 20
  *
  * 추가 요구 사항
- * 객체의 타입은 <T>로 받는다.
+ * 1. 객체의 타입은 <T>로 받는다.
  * 즉, 정해진 타입이 아닌 임의의 타입을 받을 수 있다.
- * 객체의 키는 <T>의 키를 따른다.
+ * 2. 객체의 키는 <T>의 키를 따른다.
+ * 3. 파일에 작성할 헤더를 선택할 수 있다. (headers) 기본적으로는 모든 키를 작성한다.
+ * 4. 헤더를 작성할 때, 키의 순서를 정할 수 있다.
  *
+ * 5. 빈 데이터를 받으면 에러를 던진다.
  */
 describe("csv file writer", () => {
-  describe("default csv file writer test", () => {
-    describe("writeCsvFile", () => {
-      let fs: Fs;
-      let csvFileWriter: CsvFileWriter;
+  let fs: Fs;
+  let csvFileWriter: CsvFileWriter;
+  type TestCase<T extends object> = {
+    _test: string;
+    data: T[];
+    headers?: (keyof T)[];
+    expected: string;
+  };
 
-      beforeEach(() => {
-        fs = {
-          writeFileSync: jest.fn(),
-        };
-        csvFileWriter = new CsvFileWriter(fs);
-      });
+  // Arrange
+  beforeEach(() => {
+    fs = {
+      writeFileSync: jest.fn(),
+    };
+    csvFileWriter = new CsvFileWriter(fs);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("test users", () => {
+    const users = testObjFactory().createUsers(3);
+    const testCases: TestCase<User>[] = [
+      {
+        _test: "users without headers",
+        data: users,
+        expected: "_id,name,age\n0,name0,0\n1,name1,1\n2,name2,2",
+      },
+      {
+        _test: "users with all headers",
+        data: users,
+        headers: ["_id", "name", "age"],
+        expected: "_id,name,age\n0,name0,0\n1,name1,1\n2,name2,2",
+      },
+      {
+        _test: "users with some headers",
+        data: users,
+        headers: ["_id", "name"],
+        expected: "_id,name\n0,name0\n1,name1\n2,name2",
+      },
+      {
+        _test: "users with random headers",
+        data: users,
+        headers: ["name", "_id", "age"],
+        expected: "name,_id,age\nname0,0,0\nname1,1,1\nname2,2,2",
+      },
+    ];
+
+    test.each(testCases)("test: $_test", ({ data, headers, expected }) => {
+      // Act
+      csvFileWriter.writeCsvFile("test.csv", data, headers);
+      // Assert
+      expect(fs.writeFileSync).toHaveBeenCalledWith("test.csv", expected);
+    });
+  });
+
+  describe("data is empty (Error case)", () => {
+    it("should throw error when data empty and header not empty", () => {
+      // Arrange
+      const data: User[] = [];
+      const headers: (keyof User)[] = ["_id", "name", "age"];
+      // Act
+      // Assert
+      expect(() =>
+        csvFileWriter.writeCsvFile("test.csv", data, headers)
+      ).toThrow("data is empty");
+    });
+
+    it("should throw error when data empty and header is undefined", () => {
+      // Arrange
+      const data: User[] = [];
+      const headers: (keyof User)[] | undefined = undefined;
+      // Act
+      // Assert
+      expect(() =>
+        csvFileWriter.writeCsvFile("test.csv", data, headers)
+      ).toThrow("data is empty");
+    });
+
+    it("should throw error when data empty and header is null", () => {
+      // Arrange
+      const data: User[] = [];
+      const headers: (keyof User)[] | null = null;
+      // Act
+      // Assert
+      expect(() =>
+        csvFileWriter.writeCsvFile("test.csv", data, headers!)
+      ).toThrow("data is empty");
+    });
+
+    it("should throw error when data empty and header is empty", () => {
+      // Arrange
+      const data: User[] = [];
+      const headers: (keyof User)[] = [];
+      // Act
+      // Assert
+      expect(() =>
+        csvFileWriter.writeCsvFile("test.csv", data, headers)
+      ).toThrow("data is empty");
     });
   });
 });
+interface User {
+  _id: string;
+  name: string;
+  age: number;
+}
+
+interface Post {
+  _id: string;
+  title: string;
+  content: string;
+  userId: string;
+}
+
+function createUsers(num: number): User[] {
+  return Array.from({ length: num }, (_, i) => ({
+    _id: `${i}`,
+    name: `name${i}`,
+    age: i,
+  }));
+}
+function createPosts(num: number): Post[] {
+  return Array.from({ length: num }, (_, i) => ({
+    _id: `${i}`,
+    title: `title${i}`,
+    content: `content${i}`,
+    userId: `${i}`,
+  }));
+}
+
+function testObjFactory() {
+  return { createUsers, createPosts };
+}
